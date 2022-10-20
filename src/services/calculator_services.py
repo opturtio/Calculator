ddbadbcd6ec059907bfe4e779f0bc8017d5c4edf
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-10-20 23:05:29 +0300

    Creating error messages

--- src/services/calculator_services.py
+++ src/services/calculator_services.py
@@ -4,56 +4,115 @@ class CalculatorServices:
     """Luokka hoitaa laskimen toiminnot"""
     
     def __init__(self):
-        self._current = ""
-        self._number = None
+        """
+        Konstruktori
         
-    def error(self, sign):
+        Args:
+        
+        """
+        
+        self._calculation = ""
+        self._left_brackets = 0
+        self._right_brackets = 0
+        self._signs = "+-*/."
+        self._error_message = False
+        
+    def _handle_error(self, entry, sign):
         """Informs if error occurs"""
-        pass
+        if sign in self._signs:
+            self._error_message = True
+            entry.delete(0, 'end')
+            entry.insert(0, f"You cannot enter two {sign} in a row")
+    
+    def _check_error_message(self, entry):
+        if self._error_message:
+            entry.delete(0, 'end')
+            entry.insert(0, str(self._calculation))
+            self._error_message = False
     
     def _add_number(self, entry, number):
         """Adds plus sign to calculation"""
-        self._current = entry.get()
+        self._check_error_message(entry)
+        self._calculation = entry.get()
+        self._calculation += str(number)
         entry.delete(0, 'end')
-        entry.insert(0, str(self._current) + str(number))
-        print(self._current)
+        entry.insert(0, self._calculation)
     
-    def _button_click_add(self):
+    def _button_click_add(self, entry):
         """Adds plus sign to calculation"""
-        pass
+        self._check_error_message(entry)
+        if self._calculation[-1] in self._signs:
+            self._handle_error(entry, "+")
+            return
+        self._calculation = entry.get()
+        entry.delete(0, 'end')
+        self._calculation += "+"
+        entry.insert(0, self._calculation)
     
-    def _button_click_sub(self):
+    def _button_click_sub(self, entry):
         """Adds minus sign to calculation"""
-        pass
+        self._check_error_message(entry)
+        if self._calculation[-1] in self._signs:
+            self._handle_error(entry, "-")
+            return
+        self._calculation = entry.get()
+        entry.delete(0, 'end')
+        self._calculation += "-"
+        entry.insert(0, self._calculation)
     
-    def _button_click_mul(self):
+    def _button_click_mul(self, entry):
         """Adds multiplication sign to calculation"""
-        pass
+        self._check_error_message(entry)
+        if self._calculation[-1] in self._signs:
+            self._handle_error(entry, "*")
+            return
+        self._calculation = entry.get()
+        entry.delete(0, 'end')
+        self._calculation += "*"
+        entry.insert(0, self._calculation)
     
-    def _button_click_div(self):
+    def _button_click_div(self, entry):
         """Adds division sign to calculation"""
-        pass
+        self._check_error_message(entry)
+        if self._calculation[-1] in self._signs:
+            self._handle_error(entry, "/")
+            return
+        self._calculation = entry.get()
+        entry.delete(0, 'end')
+        self._calculation += "/"
+        entry.insert(0, self._calculation)
     
-    def _left_bracket(self):
+    def _button_click_point(self, entry):
+        """Adds point to calculation"""
+        self._check_error_message(entry)
+        if self._calculation[-1] in self._signs:
+            self._handle_error(entry, ".")
+            return
+        self._calculation = entry.get()
+        entry.delete(0, 'end')
+        self._calculation += "."
+        entry.insert(0, self._calculation)
+        
+    def _left_bracket(self, entry):
         """Adds left bracket to calculation"""
         pass
     
-    def _right_bracket(self):
+    def _right_bracket(self, entry):
         """Adds right bracket to calculation"""
         pass
     
     def _button_click_C(self, entry):
         """Deletes the whole calculation"""
         entry.delete(0, 'end')
+        self._calculation = ""
     
     def _button_click_CE(self, entry):
         """Cleares one number"""
-        entry.delete(0)
+        self._calculation = entry.get()
+        entry.delete(0, 'end')
+        self._calculation = self._calculation[:-1]
+        entry.insert(0, self._calculation)
     
-    def _button_click_point(self):
-        """Adds point to calculation"""
-        pass
-    
-    def _button_click_equal(self):
+    def _button_click_equal(self, entry):
         """Calculates the calculation"""
-        pass
+
