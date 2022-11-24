5e42bd95e6621e7aefa08bf0604cccdab9d4e0ab
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-11-24 21:40:07 +0200

    changed database path to src/data

--- src/config.py
+++ src/config.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 dirname = os.path.dirname(__file__)
 
 try:
-    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
+    load_dotenv(dotenv_path=os.path.join(dirname, ".env"))
 except FileNotFoundError:
     pass
 
 DATABASE_FILENAME = os.getenv("DATABASE_FILENAME") or "database.sqlite"
-DATABASE_FILE_PATH = os.path.join(dirname, "..", DATABASE_FILENAME)
+DATABASE_FILE_PATH = os.path.join(dirname, "data", DATABASE_FILENAME)
+
+print(DATABASE_FILENAME, DATABASE_FILE_PATH)
