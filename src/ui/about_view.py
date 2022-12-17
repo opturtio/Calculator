a2ea370d0776619477a87b85f204c5c716a320e9
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-12-17 23:12:41 +0200

    Photo added to About-view

--- src/ui/about_view.py
+++ src/ui/about_view.py
@@ -1,12 +1,11 @@
 from tkinter import *
-import cv2
+from PIL import ImageTk
+import PIL.Image
 
-
-def create_image(path, resize=False):
-    image = cv2.imread(path, cv2.IMREAD_ANYCOLOR)
-    if resize:
-        return image.resize(40, 30)
-    return image
+def create_image(path):
+    image = PIL.Image.open(path)
+    tk_image = ImageTk.PhotoImage(image)
+    return tk_image
 
 
 class AboutView:
@@ -14,25 +13,26 @@ class AboutView:
 
     def __init__(self):
         self._about_window = None
-        # TODO kuvan saaminen pythoniin on todella vaikeaa :D
-        self._calculator_image = create_image('./src/assets/calculator2.png')
 
     def show_about_view(self):
         """Muodostaa about-ikkunan"""
         self._about_window = Toplevel()
         self._about_window.title("About")
-        self._about_window.geometry('300x300')
+        self._about_window.geometry('400x400')
+        self._calculator_image = create_image('./src/assets/calculator2.gif')
 
     def print_info(self):
         """Tulostaa tiedot about-ikkunaan"""
         Label(self._about_window, text='Calculator', font=(
             'Helvetica', 18, 'bold')).pack(padx=30, pady=10)
+
         Label(self._about_window, text='Calculator for simple calculation',
               font=('Helvetica', 10)).pack()
+
+        print(self._calculator_image)
+        Label(self._about_window, image=self._calculator_image).pack(side = TOP)
         Label(self._about_window, text='Author: Olli-Pekka Turtio').pack()
         Label(self._about_window, text='Published: 2022©').pack()
-        Label(self._about_window, image=self._calculator_image).pack(
-            padx=30, pady=35)
 
 
 about_view = AboutView()
