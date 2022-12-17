e0373beb110892a4d8f859aff198c854e2242341
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-12-17 15:46:30 +0200

    fixing history service

--- src/services/history_service.py
+++ src/services/history_service.py
@@ -15,15 +15,15 @@ class HistoryService:
     def __init__(self, calculation_repository=default_calculation_repository, calculation_manager=default_calculation_manager):
         self._calculation_repository = calculation_repository
         self._calculation_manager = calculation_manager
-        self._calculator_service = CalculatorService
+        self._calculator_service = CalculatorService()
 
     def replace_current_calculation_with_selected(self, listbox):
         selected = listbox.get(ACTIVE)
         selected = selected.split(": ")[1]
         selected = selected.split("=")[0]
-        print(selected)
+
         self._calculation_manager.insert_calculation(selected)
-        #self._calculator_service.insertt()
+        self._calculator_service.insert()
 
 
     def delete_calculation_from_history_view(self, listbox):
